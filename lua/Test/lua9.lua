b3de12735b68578e975481e61ca6ed328ca21f19
JimLyGH <1072594307@qq.com>
2017-01-18 13:09:00 +0800

    1

--- lua/Test/lua9.lua
+++
@@ -1,8 +0,0 @@
-local str = "||w||||gg|||";
-
-print(string.gsub(str, "/|/", "| |"));
-
---print(string.gsub(str, "||", "| |"));
---print(string.gsub(string.gsub(str, "||", "| |"), "||", "| |"));
-
-
