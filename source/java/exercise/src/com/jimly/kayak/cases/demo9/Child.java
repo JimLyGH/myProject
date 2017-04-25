b68ab0be83e5a6ff70bab120737bbb7f7f0021e0
JimLyGH <1072594307@qq.com>
2017-04-25 14:44:29 +0800

    1

---
+++ source/java/exercise/src/com/jimly/kayak/cases/demo9/Child.java
@@ -0,0 +1,40 @@
+package com.jimly.kayak.cases.demo9;
+
+public class Child extends Parent{
+
+	private String name = "child";
+	
+	public Child() {
+		tellName();
+		printName();
+	}
+	
+	public void tellName() {
+		System.out.println("Child tell:" + name);
+	}
+	
+	public void printName() {
+		System.out.println("Child print:" + name);
+	}
+	
+	
+}
+
+class Parent {
+	
+	private String name = "parent";
+	
+	public Parent() {
+		tellName();
+		printName();
+	}
+	
+	public void tellName() {
+		System.out.println("Parent tell:" + name);
+	}
+	
+	public void printName() {
+		System.out.println("Parent print:" + name);
+	}
+	
+}
